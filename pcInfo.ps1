188fffc1425f14a8f2cbec01f0993a047b91e865
DanTheDev27 <153468216+DanTheDev27@users.noreply.github.com>
2025-02-23 00:41:53 -0500

    Update pcInfo.ps1

--- pcInfo.ps1
+++ pcInfo.ps1
@@ -8,6 +8,8 @@ function Get-PCInfo {
     
     # Get computer information
     $computerInfo = Get-ComputerInfo
+    $computerSystem = Get-WmiObject Win32_ComputerSystem
+    $computerDiskUtilization = Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DeviceID='C:'"
 
     # Format the output for computer information
     $osName = $computerInfo.OsName
@@ -27,10 +29,23 @@ function Get-PCInfo {
     $getWiFiNetworks = netsh wlan show networks mode=bssid | Select-String "SSID"
     $getEthernetNetwork = Get-NetAdapter | Where-Object { $_.Status -eq 'Up' -and $_.InterfaceDescription -like '*Ethernet*' } | Select-Object Name
     $getEthernetDetailed = Get-NetIPConfiguration | Where-Object { $_.InterfaceAlias -like '*Ethernet*' }
+
     $printerInfo = Get-WmiObject -Class Win32_Printer | Select-Object Name, PortName
     $computerRamInfo = Get-WmiObject -Class Win32_PhysicalMemory | Select-Object Manufacturer, Capacity, Speed, PartNumber
-    $ram = (computerInfo).TotalPhysicalMemory
-    $ramInGB = $ramInGB = [math]::round($ram / 1GB, 2)
+    $ramInGB = [math]::round($computerSystem.TotalPhysicalMemory / 1GB, 2)
+    # Get the total space and free space in gigabytes
+    $totalSpaceGB = [math]::Round($computerDiskUtilization.Size / 1GB, 2)
+    $freeSpaceGB = [math]::Round($computerDiskUtilization.FreeSpace / 1GB, 2)
+
+    # Calculate the free space percentage
+    $freeSpacePercent = [math]::Round(($freeSpaceGB / $totalSpaceGB) * 100, 2)
+
+    #ping router look for devices under your ip
+    #use test connection and dnsresolve which will get the device name
+
+    #returns ip and mac of devices on network, only work with devices you have communicated with
+    #Get-NetNeighbor -AddressFamily IPv4 | Select-Object IPAddress, LinkLayerAddress
+
 
 
 
@@ -58,8 +73,12 @@ function Get-PCInfo {
     Write-Output "Number of users on computer: $osNumberOfUsers"
     Write-Output "Users: $allUsers"
     write-Output "WiFi Networks: $getWiFiNetworks"
-    Write-Output $ram
+    Write-Output "RAM: $ramInGB GB"
+    Write-Output "Total C: drive space $totalSpaceGB"
+    Write-Output "Free C: drve space $freeSpaceGB"
+    Write-Output "Free Space left: $freeSpacePercent%"
 }
 
 # Call the function to see the output
+Get-PCInfo
 Get-PCInfo
